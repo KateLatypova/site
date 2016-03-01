b9602653322b607bc3218f5898f9258568780c64
Savi <Ultimate95@mail.ru>
2016-03-01 20:41:01 +0500

    add form for comment

---
+++ Site/gallery/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django_markdown.widgets import MarkdownWidget
+from gallery.models import CommentForImage
+
+
+class CommentForm(forms.ModelForm):
+    comment = forms.CharField(max_length=256, widget=MarkdownWidget(attrs={'maxlength': 256}))
+
+    class Meta:
+        model = CommentForImage
+        fields = ['comment']
